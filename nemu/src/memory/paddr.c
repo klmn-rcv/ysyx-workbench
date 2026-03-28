81b812f7a29a85aef37fd9a0dc9945859d4bbf07
Honglu Ji <jihonglu23@mails.ucas.ac.cn>
2026-03-28 21:44:03 +0800

    feat: add mtrace (configurable via menuconfig)

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,6 +51,10 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+#ifdef CONFIG_MTRACE
+  if(CONFIG_MTRACE_COND)
+    printf("[mtrace] paddr_read:  addr = " FMT_PADDR ", len = %d\n", addr, len);
+#endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -58,6 +62,10 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  if(CONFIG_MTRACE_COND)
+    printf("[mtrace] paddr_write: addr = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data);
+#endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
