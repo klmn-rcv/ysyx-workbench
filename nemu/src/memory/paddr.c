3abc494b1d9b818b94d40e02d83e8653b0c16548
Honglu Ji <jihonglu23@mails.ucas.ac.cn>
2026-04-03 23:25:47 +0800

    feat: implement input IOE

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -28,11 +28,19 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
+#ifdef CONFIG_MTRACE
+  if(CONFIG_MTRACE_COND)
+    printf("[mtrace] pmem_read: addr = " FMT_PADDR ", len = %d\n", addr, len);
+#endif
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  if(CONFIG_MTRACE_COND)
+    printf("[mtrace] pmem_write: addr = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data);
+#endif
   host_write(guest_to_host(addr), len, data);
 }
 
@@ -51,22 +59,19 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-#ifdef CONFIG_MTRACE
-  if(CONFIG_MTRACE_COND)
-    printf("[mtrace] paddr_read:  addr = " FMT_PADDR ", len = %d\n", addr, len);
-#endif
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_pmem(addr))) {
+    return pmem_read(addr, len);
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-#ifdef CONFIG_MTRACE
-  if(CONFIG_MTRACE_COND)
-    printf("[mtrace] paddr_write: addr = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data);
-#endif
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_pmem(addr))) {  
+    pmem_write(addr, len, data); 
+    return; 
+  }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
