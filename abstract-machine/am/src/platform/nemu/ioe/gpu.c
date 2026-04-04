5598ad470bc360a6156c5a36f5737402aad2dd98
Honglu Ji <jihonglu23@mails.ucas.ac.cn>
2026-04-04 14:19:52 +0800

    feat: implement VGA IOE

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,17 +4,37 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  uint32_t wh = inl(VGACTL_ADDR);
+  int w = wh >> 16;
+  int h = wh & 0xffff;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t wh = inl(VGACTL_ADDR);
+  int w = wh >> 16;
+  int h = wh & 0xffff;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = w, .height = h,
+    .vmemsz = w * h * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t wh = inl(VGACTL_ADDR);
+  int screen_w = wh >> 16;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int start = ctl->y * screen_w + ctl->x;
+  for(int i = 0; i < ctl->h; i++) {
+    for(int j = 0; j < ctl->w; j++) {
+      fb[start + i * screen_w + j] = pixels[i * ctl->w + j];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
