13e2aaf0700cef92ee304d9569c483436a93a716
tracer-ysyx <tracer@ysyx.org>
2026-05-10 12:22:14 +0800

    > compile NEMU 2023K8009916002 纪弘璐 Linux ysyx 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:22:14 up  1:16,  1 user,  load average: 0.25, 0.17, 0.10

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -7,10 +7,30 @@ extern char _heap_end;
 int main(const char *args);
 
 #define UART_BASE 0x10000000L
-#define UART_TX   0
+#define UART_RX   0   // R
+#define UART_TX   0   // W
+#define UART_IER  1   // R/W
+#define UART_IIR  2   // R
+#define UART_FCR  2   // W
+#define UART_LCR  3   // R/W
+#define UART_MCR  4   // W
+#define UART_LSR  5   // R
+#define UART_MSR  6   // R
+
+#define UART_DLLO 0   // R/W
+#define UART_DLHI 1   // R/W
 
 Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
+
+void init_uart(void) {
+  *(volatile uint8_t *)(UART_BASE + UART_LCR) = 0x80; // set DLAB=1 to access baud rate divisor
+  *(volatile uint8_t *)(UART_BASE + UART_DLLO) = 0x01; // set baud rate divisor to 1 (115200 baud)
+  *(volatile uint8_t *)(UART_BASE + UART_DLHI) = 0x00; // set baud rate divisor to 1 (115200 baud)
+  *(volatile uint8_t *)(UART_BASE + UART_LCR) = 0x03; // set 8N1 format (8 data bits, no parity, 1 stop bit)
+  *(volatile uint8_t *)(UART_BASE + UART_FCR) = 0xC6; // clear FIFO and set trigger level to 14 bytes (Bit 0 is ignored in 16550)
+  *(volatile uint8_t *)(UART_BASE + UART_IER) = 0x00; // disable all interrupts
+}
 
 void putch(char ch) {
   *(volatile uint8_t *)(UART_BASE + UART_TX) = ch;
@@ -27,6 +47,8 @@ void _trm_init() {
   // asm volatile("csrr %0, mvendorid" : "=r"(mvendorid));
   // asm volatile("csrr %0, marchid" : "=r"(marchid));
   // printf("mvendorid = 0x%x, marchid = 0x%x\n", mvendorid, marchid);
+
+  init_uart();
 
   int ret = main(mainargs);
   halt(ret);
