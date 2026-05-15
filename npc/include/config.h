8dc3e8f0210696062fa1163a8363c9cf8e9f9dc4
tracer-ysyx <tracer@ysyx.org>
2026-05-15 21:32:11 +0800

    > compile NEMU 2023K8009916002 纪弘璐 Linux ysyx 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:32:11 up  1:12,  1 user,  load average: 0.76, 0.67, 0.38

--- npc/include/config.h
+++ npc/include/config.h
@@ -2,11 +2,11 @@
 #define __CONFIG_H__
 
 // #define CONFIG_GEN_WAVE 1
-#define CONFIG_ITRACE 1
+// #define CONFIG_ITRACE 1
 // #define CONFIG_MTRACE 1
 // #define CONFIG_DTRACE 1
 // #define CONFIG_FTRACE 1
-#define CONFIG_DIFFTEST 1
+// #define CONFIG_DIFFTEST 1
 // #define CONFIG_DEBUGPOINT 1
 
 #define CONFIG_RV32 1
