5fc120a63b74ee1aa58f9e7610f571a4f67242f2
tracer-ysyx <tracer@ysyx.org>
2026-05-17 03:23:26 +0800

    > compile NEMU 2023K8009916002 纪弘璐 Linux ysyx 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  03:23:26 up 15:32,  1 user,  load average: 0.35, 0.27, 0.16

--- ysyxSoC/src/device/PSRAM.scala
+++ ysyxSoC/src/device/PSRAM.scala
@@ -30,7 +30,55 @@ class psram extends BlackBox {
 
 class psramChisel extends RawModule {
   val io = IO(Flipped(new QSPIIO))
-  val di = TriStateInBuf(io.dio, 0.U, false.B) // change this if you need
+  withClockAndReset(io.sck.asClock, io.ce_n.asAsyncReset) {
+    val dout = Wire(UInt(4.W))
+    val out_en = Wire(Bool())
+    val di = TriStateInBuf(io.dio, dout, out_en)
+
+    val mem = Mem(0x400000, UInt(8.W))
+
+    val counter = RegInit(0.U(5.W))
+    val FINAL_COUNT = 28
+
+    when(counter <= FINAL_COUNT.U) {
+      counter := counter + 1.U
+    }
+
+    val cmd_trasmitting = counter <= 7.U && !io.ce_n // && FSM.state === FSM.State.BUSY
+    val addr_trasmitting = (counter >= 8.U) && (counter <= 13.U)
+    val data_trasmitting = (counter >= 21.U) && (counter <= FINAL_COUNT.U)
+
+    out_en := data_trasmitting
+
+    // val done = counter === FINAL_COUNT.U + 1.U
+
+    val cmd = RegInit(0.U(8.W))
+    val addr = RegInit(0.U(24.W))
+    val data = mem(addr)
+
+    when(cmd_trasmitting) {
+      cmd := Cat(cmd(6, 0), di(0))
+    }
+
+    when(addr_trasmitting) {
+      addr := Cat(addr(19, 0), di)
+    }
+
+    when(counter === 8.U) {
+      assert(cmd === "heb".U, "Only Fast Read Quad I/O (0xEB) command is supported now")
+    }
+
+    dout := MuxLookup(counter - 21.U, data(3,0))(Seq(
+      0.U -> data(7,4),
+      1.U -> data(3,0),
+      2.U -> data(15,12),
+      3.U -> data(11,8),
+      4.U -> data(23,20),
+      5.U -> data(19,16),
+      6.U -> data(31,28),
+      7.U -> data(27,24)
+    ))
+  }
 }
 
 class APBPSRAM(address: Seq[AddressSet])(implicit p: Parameters) extends LazyModule {
