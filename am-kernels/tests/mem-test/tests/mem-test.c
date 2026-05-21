a50f1bfac9c9409a975ab9e1b310c7952bddc58a
tracer-ysyx <tracer@ysyx.org>
2026-05-21 21:17:00 +0800

    > compile NEMU 2023K8009916002 纪弘璐 Linux ysyx 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:17:00 up 1 day, 18:09,  1 user,  load average: 2.49, 2.33, 2.55

--- am-kernels/tests/mem-test/tests/mem-test.c
+++ am-kernels/tests/mem-test/tests/mem-test.c
@@ -13,7 +13,7 @@ void test8(uintptr_t start, uintptr_t end) {
   }
   printf("test8: write done\n");
   for (uintptr_t addr = start; addr < end; addr++) {
-    printf("test8: start reading addr = 0x%08lx\n", (unsigned long)addr);
+    printf("test8: start reading addr = 0x%x\n", (unsigned long)addr);
     uint8_t expect = (uint8_t)(addr & 0xff);
     uint8_t got = *(volatile uint8_t *)addr;
     if (got != expect) {
