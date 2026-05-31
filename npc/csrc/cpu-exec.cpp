551469743dce0661e496a47c13154ec27eed356a
tracer-ysyx <tracer@ysyx.org>
2026-05-31 21:33:09 +0800

    > compile NEMU 2023K8009916002 纪弘璐 Linux ysyx 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:33:09 up 1 day,  7:30,  1 user,  load average: 0.47, 0.87, 1.00

--- npc/csrc/cpu-exec.cpp
+++ npc/csrc/cpu-exec.cpp
@@ -18,7 +18,7 @@
  * You can modify this value as you want.
  */
 #define MAX_INST_TO_PRINT 10
-#define MAX_INST_TO_LOG 1000000
+#define MAX_INST_TO_LOG 100000
 
 static uint64_t g_nr_inst = 0;
 static uint64_t g_timer = 0; // unit: us
@@ -131,8 +131,9 @@ static void statistic() {
   Log("host time spent = %llu us", (unsigned long long)g_timer);
   Log("total guest instructions = %llu", (unsigned long long)g_nr_inst);
   if (g_timer > 0) {
-    Log("simulation frequency = %llu inst/s",
-        (unsigned long long)(g_nr_inst * 1000000 / g_timer));
+    Log("simulation frequency = %llu inst/s, IPC = %.2f",
+        (unsigned long long)(g_nr_inst * 1000000 / g_timer),
+        (float)g_nr_inst / (float)npc_state.cycles);
   } else {
     Log("Finish running in less than 1 us and can not calculate the simulation frequency");
   }
